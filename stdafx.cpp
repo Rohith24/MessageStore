cad74fc3d3e7b71f76b44e51527241def0ffdcc4
Rohith Reddy Challa <Rohith24@users.noreply.github.com>
2016-07-16 06:15:38 +0530

    Client Files

--- stdafx.cpp
+++ stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// socket.pch will be the pre-compiled header
+// socket_client.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
